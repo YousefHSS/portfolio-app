4b6a67e032cf263cba3d8603f968ffb1012443d2
yousefhussen <yousefhussen139@gmail.com>
2024-08-02 15:21:13 +0300

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { HeaderSectionComponent } from "./header-section/header-section.component";
+import { AboutSectionComponent } from "./about-section/about-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, HeaderSectionComponent, AboutSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
